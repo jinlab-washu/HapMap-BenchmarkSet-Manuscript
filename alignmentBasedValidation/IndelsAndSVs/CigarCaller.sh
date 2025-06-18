245c3305e140834cea125607438126f9de16785e
Andrew Ruttenberg <161983234+andrewruttenberg@users.noreply.github.com>
2025-06-18 11:12:01 -0400

    Update CigarCaller.sh

--- alignmentBasedValidation/IndelsAndSVs/CigarCaller.sh
+++ alignmentBasedValidation/IndelsAndSVs/CigarCaller.sh
@@ -1,10 +1,10 @@
 numberArguments=$#
 
-if [ "$numberArguments" -ne 5 ] && [ "$numberArguments" -ne 6 ]
+if [ "$numberArguments" -ne 6 ]
 then
     echo "Incorrect number of arguments"
     echo "${numberArguments} arguments given"
-    echo "Correct usage: bash BetterMpileup.sh {Indel/SV} {Bam File} {Reference File} {if LRS or Not} {run name} {truthset (optional)}"
+    echo "Correct usage: bash BetterMpileup.sh {Indel/SV} {Bam File} {Reference File} {if LRS or Not (True/False)} {run name} {truthset} {Out Directory}"
     exit 53
 fi
 
@@ -13,8 +13,8 @@ bam=$2
 ref=$3
 LRS=$4
 name=$5
-defaultTruth="/storage1/fs1/jin810/Active/testing/Ruttenberg/SMAHT/References/Truthsets/${type}/${type}_*.vcf"
-truth=${6:-$defaultTruth}
+truth=$6
+wkdir=$7
 
 if [[ "$LRS" != "True" && "$LRS" != "False" ]]; then
     echo "LRS is neither True nor False"
@@ -32,28 +32,15 @@ then
     exit 53478
 fi
 
-wkdir="/storage1/fs1/jin810/Active/testing/Ruttenberg/SMAHT/${type}/CigarOutput"
 outdir="${wkdir}/${name}"
 mkdir $outdir
 mkdir "$outdir/outputVCFs"
-samdir="$outdir/samfiles" 
-mkdir $samdir
+mkdir "$outdir/samfiles" 
 
 indel=$([ "$type" == "Indel" ] && echo True || echo False)
 
-if [ "$numberArguments" -eq 5 ];
-then
-    echo "default truthset"
-    for file in $truth; do
-        chrom=`basename $file | awk -F'_' '{print $2}' | awk -F '.' '{print $1}'`
-        mkdir $outdir/$chrom 
-        bsub -G compute-jin810 -q general -g /ruttenberg.andrew/FiftyJobs -o "${outdir}/${chrom}/mpile.log" -e "${outdir}/${chrom}/mpile.err" -R 'rusage[mem=100GB]' -M 400B -a 'docker(anman1227/rutt_basic:vs4)' bash /storage1/fs1/jin810/Active/testing/Ruttenberg/Code/FinalSMAHTCode/CigarCaller/CigarChecker.sh $file $bam $ref $LRS $indel "${outdir}/${chrom}" "${outdir}/outputVCFs/${chrom}.vcf"
-    done
-elif [ "$numberArguments" -eq 6 ];
-then
-    echo "truthset given"
-    mkdir $outdir/truthsets
-    for chrom in chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chr20 chr21 chr22 chrX chrY; do
-        bsub -G compute-jin810 -q general -g /ruttenberg.andrew/FiftyJobs -o "${outdir}/${chrom}_mpile.log" -e "${outdir}/${chrom}_mpile.err" -R 'rusage[mem=100GB]' -M 400GB -a 'docker(anman1227/rutt_basic:vs4)' bash /storage1/fs1/jin810/Active/testing/Ruttenberg/Code/FinalSMAHTCode/CigarCaller/CigarChecker.sh $truth $bam $ref $LRS $indel "${outdir}" "${outdir}/outputVCFs/${name}_${chrom}.vcf" $chrom
-    done
-fi
+mkdir $outdir/truthsets
+for chrom in chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chr20 chr21 chr22 chrX chrY; do
+    bash /storage1/fs1/jin810/Active/testing/Ruttenberg/Code/FinalSMAHTCode/CigarCaller/CigarChecker.sh $truth $bam $ref $LRS $indel "${outdir}" "${outdir}/outputVCFs/${name}_${chrom}.vcf" $chrom
+done
+
