42c95f11d81121aebf6e368bc50915bb5c4affd6
NahyunKong <91095452+NahyunKong@users.noreply.github.com>
2026-04-14 10:54:48 -0500

    Update Figure S4-S5.R

--- manuscriptFigures/supplementaryFigures/Figure S4-S5.R
+++ manuscriptFigures/supplementaryFigures/Figure S4-S5.R
@@ -6,55 +6,6 @@ install.packages("patchwork")
 library(patchwork)
 library(VariantAnnotation)
 
-########################################## Fig 4A5A ##########################################
-
-chromosome_order <- c(paste0("chr", 1:22), "chrX", "chrY")
-variant_df$Chromosome <- factor(variant_df$Chromosome, levels = chromosome_order)
-
-variant_colors <- c(SNV = "#0077BB", Indel = "#EE7733", SV = "#009988")
-
-variant_df$Variant_Type <- factor(variant_df$Variant_Type, levels = c("SV", "Indel", "SNV"))
-
-plot_all_variants <- ggplot(variant_df, aes(x = Chromosome, y = Variant_Count, fill = Variant_Type)) +
-  geom_bar(stat = "identity", position = "stack") +
-  scale_fill_manual(values = variant_colors) +
-  scale_y_continuous(labels = comma) +  # Remove scientific notation
-  labs(title = "Variants by Chromosome",
-       x = "Chromosome", y = "Variant Count") +
-  theme_minimal() +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 10),
-        axis.text.y = element_text(size = 10),
-        axis.title.x = element_text(size = 12),
-        axis.title.y = element_text(size = 12),
-        plot.title = element_text(size = 18, face = "bold", hjust = 0.5),
-        legend.title = element_blank(),
-        panel.grid = element_blank())
-
-########################################## Fig 4B5B ##########################################
-
-mutation_type_order <- unique(melted_df$Mutation_Type)
-melted_df$Mutation_Type <- factor(melted_df$Mutation_Type, levels = mutation_type_order)
-
-ggplot(melted_df, aes(x = Mutation_Type, y = Count, fill = Tissue_type)) +
-  geom_bar(stat = "identity", position = position_dodge(), fill = "#0077BB", 
-           width = 0.7) +
-  labs(title = "SNV Variant Counts by Mutation Type",
-       x = "Mutation Type", y = "Count",
-       fill = "Tissue Type") +
-  scale_y_continuous(labels = scales::label_number(accuracy = 1e4)) +
-  theme_minimal() +
-  theme(axis.text.x = element_text(hjust = 0.5, size = 11),
-        axis.text.y = element_text(size = 11),
-        axis.title.x = element_text(size = 13),
-        axis.title.y = element_text(size = 13),
-        plot.title = element_text(size = 18, face = "bold", hjust = 0.5),
-        plot.background = element_rect(fill = "transparent", color = NA),  
-        panel.background = element_rect(fill = "transparent", color = NA),
-        panel.grid = element_blank()
-
-########################################## Fig 4C5C ##########################################
-
-
 
 ########################################## Fig 4D5D ##########################################
 
